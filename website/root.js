404504a4baf60d870c5e0608d26af2b9d4d53518
rovati <53517598+rovati@users.noreply.github.com>
2023-04-23 19:06:53 +0200

    Some more stylign

---
+++ website/root.js
@@ -0,0 +1,8 @@
+function whenDocumentLoaded(action) {
+	if (document.readyState === "loading") {
+		document.addEventListener("DOMContentLoaded", action);
+	} else {
+		// `DOMContentLoaded` already fired
+		action();
+	}
+}
