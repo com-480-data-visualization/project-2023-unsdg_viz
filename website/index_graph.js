eef784d0f101c10c80b48afd6a2b21374356ab61
julia-walti <91602293+julia-walti@users.noreply.github.com>
2023-06-03 09:09:44 +0200

    Add files via upload

--- website/index_graph.js
+++ website/index_graph.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click1);
         click1++;
         if (click1 % 2 === 1) {
-            document.getElementById('id1').innerHTML = "<div id='id1', style='position:relative'> <img src='index_imgs/blanks/id1.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 15px;'>   </div> </div>";
+            document.getElementById('id1').innerHTML = "<div id='id1', style='position:relative'> <img src='index_imgs/blanks/id1.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 15%;'>   </div> </div>";
         } else {
             document.getElementById('id1').innerHTML = '<div id="id1"><img src="index_imgs/id1.png" alt="Image 2"style="width:100%"></div>';
         }
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click2);
         click2++;
         if (click2 % 2 === 1) {
-            document.getElementById('id2').innerHTML = "<div id='id2', style='position:relative'> <img src='index_imgs/blanks/id2.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 15px;'>  </div> </div>";
+            document.getElementById('id2').innerHTML = "<div id='id2', style='position:relative'> <img src='index_imgs/blanks/id2.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 100%;'>  </div> </div>";
         } else {
             document.getElementById('id2').innerHTML = '<div id="id2"><img src="index_imgs/id2.png" alt="Image 2"style="width:100%"></div>';
         }
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click13);
         click3++;
         if (click3 % 2 === 1) {
-            document.getElementById('id3').innerHTML = "<div id='id3', style='position:relative'> <img src='index_imgs/blanks/id3.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; top: 70px; left: 0px;z-index:2; font-size: 15px;'> Mortality rate, PM2.5 air pollution </div> </div>";
+            document.getElementById('id3').innerHTML = "<div id='id3', style='position:relative'> <img src='index_imgs/blanks/id3.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; top: 70px; left: 0px;z-index:2; font-size: 100%;'> <ul> <li>Mortality rate, </li> PM2.5 air pollution </li> </ul> </div> </div>";
         } else {
             document.getElementById('id3').innerHTML = '<div id="id3"><img src="index_imgs/id3.png" alt="Image 2"style="width:100%"></div>';
         }
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click4);
         click4++;
         if (click4 % 2 === 1) {
-            document.getElementById('id4').innerHTML = "<div id='id4', style='position:relative'> <img src='index_imgs/blanks/id4.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; top: 50px; left: 10px;z-index:2; font-size: 15px;'> IIASA rates of no education projections, Literacy rate </div> </div>";
+            document.getElementById('id4').innerHTML = "<div id='id4', style='position:relative'> <img src='index_imgs/blanks/id4.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; top: 50px; left: 10px;z-index:2; font-size: 100%;'> <ul> <li> IIASA rates of no education projections, </li> Literacy rate </li> </ul> </div> </div>";
         } else {
             document.getElementById('id4').innerHTML = '<div id="id4"><img src="index_imgs/id4.png" alt="Image 2"style="width:100%"></div>';
         }
@@ -64,7 +64,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click5);
         click5++;
         if (click5 % 2 === 1) {
-            document.getElementById('id5').innerHTML = "<div id='id5', style='position:relative'> <img src='index_imgs/blanks/id5.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 15px;'>   </div> </div>";
+            document.getElementById('id5').innerHTML = "<div id='id5', style='position:relative'> <img src='index_imgs/blanks/id5.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 100%;'>   </div> </div>";
         } else {
             document.getElementById('id5').innerHTML = '<div id="id5"><img src="index_imgs/id5.png" alt="Image 2"style="width:100%"></div>';
         }
@@ -78,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click6);
         click6++;
         if (click6 % 2 === 1) {
-            document.getElementById('id6').innerHTML = "<div id='id6', style='position:relative'> <img src='index_imgs/blanks/id6.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 15px;'>   </div> </div>";
+            document.getElementById('id6').innerHTML = "<div id='id6', style='position:relative'> <img src='index_imgs/blanks/id6.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 100%;'>   </div> </div>";
         } else {
             document.getElementById('id6').innerHTML = '<div id="id6"><img src="index_imgs/id6.jpg" alt="Image 2"style="width:100%"></div>';
         }
@@ -92,7 +92,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click7);
         click7++;
         if (click7 % 2 === 1) {
-            document.getElementById('id7').innerHTML = "<div id='id7', style='position:relative'> <img src='index_imgs/blanks/id7.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; top: 10px; left: 0px; z-index=2; font-size: 15px;'> Greenhousegas emissions, Proportion of population with primary relianceron clean fuels, Renewable energy share, CO2 emissions from fuel combustion, Fossil fuel subsidies consumption and production </div> </div>";
+            document.getElementById('id7').innerHTML = "<div id='id7', style='position:relative'> <img src='index_imgs/blanks/id7.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; top: 10px; left: 0px; z-index=2; font-size: 100%;'> <ul> <li> Greenhousegas emissions, </li> Proportion of population with primary relianceron clean fuels, </li> Renewable energy share, CO2 emissions from fuel combustion, </li> Fossil fuel subsidies consumption and production </li> </ul> </div> </div>";
         } else {
             document.getElementById('id7').innerHTML = '<div id="id7"><img src="index_imgs/id7.png" alt="Image 2"style="width:100%"></div>';
         }
@@ -106,7 +106,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click8);
         click8++;
         if (click8 % 2 === 1) {
-            document.getElementById('id8').innerHTML = "<div id='id8', style='position:relative'> <img src='index_imgs/blanks/id8.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; top: 70px; left: 5px;z-index:2; font-size: 15px;'> Total government revenue proportion of gdp, Annual growth rate </div> </div>";
+            document.getElementById('id8').innerHTML = "<div id='id8', style='position:relative'> <img src='index_imgs/blanks/id8.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; top: 70px; left: 5px;z-index:2; font-size: 100%;'> <ul> <li> Total government revenue proportion of gdp, </li> Annual growth rate </li></ul> </div> </div>";
         } else {
             document.getElementById('id8').innerHTML = '<div id="id8"><img src="index_imgs/id8.png" alt="Image 2"style="width:100%"></div>';
         }
@@ -120,7 +120,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click9);
         click9++;
         if (click9 % 2 === 1) {
-            document.getElementById('id9').innerHTML = "<div id='id9', style='position:relative'> <img src='index_imgs/blanks/id9.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; top: 80px; left: 0px;z-index:2; font-size: 15px;'> FDI inflows </div> </div>";
+            document.getElementById('id9').innerHTML = "<div id='id9', style='position:relative'> <img src='index_imgs/blanks/id9.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; top: 80px; left: 0px;z-index:2; font-size: 100%;'> <ul> <li> FDI inflows </li> </ul> </div> </div>";
         } else {
             document.getElementById('id9').innerHTML = '<div id="id9"><img src="index_imgs/id9.png" alt="Image 2"style="width:100%"></div>';
         }
@@ -134,7 +134,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click10);
         click10++;
         if (click10 % 2 === 1) {
-            document.getElementById('id10').innerHTML = "<div id='id10', style='position:relative'> <img src='index_imgs/blanks/id10.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 15px;'>  </div> </div>";
+            document.getElementById('id10').innerHTML = "<div id='id10', style='position:relative'> <img src='index_imgs/blanks/id10.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 100%;'>  </div> </div>";
         } else {
             document.getElementById('id10').innerHTML = '<div id="id10"><img src="index_imgs/id10.png" alt="Image 2"style="width:100%"></div>';
         }
@@ -148,7 +148,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click11);
         click11++;
         if (click11 % 2 === 1) {
-            document.getElementById('id11').innerHTML = "<div id='id11', style='position:relative'> <img src='index_imgs/blanks/id11.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 15px;'>   </div> </div>";
+            document.getElementById('id11').innerHTML = "<div id='id11', style='position:relative'> <img src='index_imgs/blanks/id11.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 100%;'>   </div> </div>";
         } else {
             document.getElementById('id11').innerHTML = '<div id="id11"><img src="index_imgs/id11.jpg" alt="Image 2"style="width:100%"></div>';
         }
@@ -162,7 +162,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click12);
         click12++;
         if (click12 % 2 === 1) {
-            document.getElementById('id12').innerHTML = "<div id='id12', style='position:relative'> <img src='index_imgs/blanks/id12.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 15px;'>  </div> </div>";
+            document.getElementById('id12').innerHTML = "<div id='id12', style='position:relative'> <img src='index_imgs/blanks/id12.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 100%;'>  </div> </div>";
         } else {
             document.getElementById('id12').innerHTML = '<div id="id12"><img src="index_imgs/id12.png" alt="Image 2"style="width:100%"></div>';
         }
@@ -176,7 +176,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click13);
         click13++;
         if (click13 % 2 === 1) {
-            document.getElementById('id13').innerHTML = "<div id='id13', style='position:relative'> <img src='index_imgs/blanks/id13.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; top: 5px; left: 0px;z-index:2; font-size: 15px;'> Number of companies publishing sustainability reports, Education for sustainable development, Enhance policy coherence for sustainable development, Land degraded over total land area </div> </div>";
+            document.getElementById('id13').innerHTML = "<div id='id13', style='position:relative'> <img src='index_imgs/blanks/id13.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; top: 5px; left: 0px;z-index:2; font-size: 100%;'> <ul> <li>Number of companies publishing sustainability reports, </li> Education for sustainable development, </li> Enhance policy coherence for sustainable development, </li> Land degraded over total land area </li> </ul> </div> </div>";
         } else {
             document.getElementById('id13').innerHTML = '<div id="id13"><img src="index_imgs/id13.png" alt="Image 2"style="width:100%"></div>';
         }
@@ -190,7 +190,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click1);
         click14++;
         if (click14 % 2 === 1) {
-            document.getElementById('id14').innerHTML = "<div id='id14', style='position:relative'> <img src='index_imgs/blanks/id14.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 15px;'>  </div> </div>";
+            document.getElementById('id14').innerHTML = "<div id='id14', style='position:relative'> <img src='index_imgs/blanks/id14.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 100%;'>  </div> </div>";
         } else {
             document.getElementById('id14').innerHTML = '<div id="id14"><img src="index_imgs/id14.png" alt="Image 2"style="width:100%"></div>';
         }
@@ -204,7 +204,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('check', click15);
         click15++;
         if (click15 % 2 === 1) {
-            document.getElementById('id15').innerHTML = "<div id='id15', style='position:relative'> <img src='index_imgs/blanks/id15.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 15px;'>  </div> </div>";
+            document.getElementById('id15').innerHTML = "<div id='id15', style='position:relative'> <img src='index_imgs/blanks/id15.png' style='width:100%; z-index=1'>  <div style='color:white; position:absolute; bottom: 20px; right: 20px;z-index:2; font-size: 100%;'>  </div> </div>";
         } else {
             document.getElementById('id15').innerHTML = '<div id="id15"><img src="index_imgs/id15.png" alt="Image 2"style="width:100%"></div>';
         }
